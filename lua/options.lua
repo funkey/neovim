38e1a0dd27fceffa1ece7968359b5259e1524a97
Jan Funke <funkej@janelia.hhmi.org>
2025-12-14 14:26:14 -0500

    Update neo-tree after lazygit

--- lua/options.lua
+++ lua/options.lua
@@ -63,3 +63,14 @@ opt.fillchars = {
   vertright = '┣',
   verthoriz = '╋',
 }
+
+-- update neo-tree after lazy-git
+vim.api.nvim_create_autocmd({ "BufLeave" }, {
+	pattern = { "*lazygit*" },
+	group = vim.api.nvim_create_augroup("git_refresh_neotree", {clear = true}),
+	callback = function()
+		require("neo-tree.sources.filesystem.commands").refresh(
+			require("neo-tree.sources.manager").get_state("filesystem")
+		)
+	end,
+})
